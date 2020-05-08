61205a4a8e9432d10e85c7392590505534c7c702
acferM <matheus.acfer@Nave.org.br>
2020-05-08 20:13:29 -0300

    spawn control feito, adaptação do teleporte feita, adaptação do spawn control feita

---
+++ Assets/Scripts/SpawControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawControl : MonoBehaviour
+{
+    public GameObject asteroid, screenBorder;
+    Vector3 position;
+
+    void Start()
+    {
+        InvokeRepeating("spawn", 8f, 1f);
+    }
+
+    void spawn()
+    {
+        position = new Vector3(Random.Range(-screenBorder.transform.position.x + .5f, screenBorder.transform.position.x - .5f),
+                               Random.Range(screenBorder.transform.position.y + 5, screenBorder.transform.position.y + 10));
+
+        Instantiate(asteroid, position, Quaternion.identity);
+    }
+}
--- Assets/Scripts/TeleporterScript.cs
+++ Assets/Scripts/TeleporterScript.cs
@@ -4,50 +4,43 @@ using UnityEngine;
 
 public class TeleporterScript : MonoBehaviour
 {
-    // LEMBRAR DE TESTAR TP SEM O IF
-
-    public Transform horiontalTp;
-    public Transform verticalTp;
+    public GameObject horiontalTp;
+    public GameObject verticalTp;
+    public GameObject adapter;
 
     public GameObject player;
     GameObject newPlayer;
-    public bool canClone = true;
+    public bool canClone;
 
     Vector3 clonePosition;
 
     void Update()
     {
-        // LEMBRAR DE TESTAR TP SEM O IF
-
-        if (Mathf.Abs(transform.position.x) >= 10.911f && canClone)
-       {
+        if (Mathf.Abs(transform.position.x) >= adapter.transform.position.x && canClone)
+        {
             canClone = false;
             if (transform.position.x > 0)
             {
-                clonePosition.x = horiontalTp.position.x * -1;
-                clonePosition.y = transform.position.y;
+                clonePosition = new Vector3(horiontalTp.transform.position.x * -1, transform.position.y);
                 clonar(ref newPlayer);
             }
             else
             {
-                clonePosition.x = horiontalTp.position.x;
-                clonePosition.y = transform.position.y;
+                clonePosition = new Vector3(horiontalTp.transform.position.x, transform.position.y);
                 clonar(ref newPlayer);
             }
-       }
+        }
 
-        if (transform.position.y >= 4.086f && canClone)
+        if (transform.position.y >= adapter.transform.position.y && canClone)
         {
             canClone = false;
-            clonePosition.y = verticalTp.position.y * -1;
-            clonePosition.x = transform.position.x;
+            clonePosition = new Vector3(transform.position.x, verticalTp.transform.position.y * -1);
             clonar(ref newPlayer);
         }
-        else if (transform.position.y <= -4.1189f && canClone)
+        else if (transform.position.y <= -adapter.transform.position.y - 0.0329f && canClone)
         {
             canClone = false;
-            clonePosition.y = verticalTp.position.y;
-            clonePosition.x = transform.position.x;
+            clonePosition = new Vector3(transform.position.x, verticalTp.transform.position.y);
             clonar(ref newPlayer);
         }
     }
@@ -57,14 +50,10 @@ public class TeleporterScript : MonoBehaviour
         newPlayer = Instantiate(player, clonePosition, Quaternion.identity);
     }
 
-    // LEMBRAR DE TESTAR SEM O IF
     private void OnBecameInvisible()
     {
-        if (this.gameObject.name == "hero")
-        {
-            newPlayer.name = "hero";
-            newPlayer.GetComponent<TeleporterScript>().canClone = true;
-            Destroy(this.gameObject);
-        }
+        newPlayer.name = "hero";
+        newPlayer.GetComponent<TeleporterScript>().canClone = true;
+        Destroy(this.gameObject);
     }
 }
